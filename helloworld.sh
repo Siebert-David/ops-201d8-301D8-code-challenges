e88b2fcef172162f95f9dbf8674bf226dba2caff
Siebert-David <130385666+Siebert-David@users.noreply.github.com>
2023-04-26 18:56:44 -0400

    Create helloworld.sh

---
+++ helloworld.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script Name:                  Hello World
+# Author:                       David Siebert 
+# Date of latest revision:      04/26/2023
+# Purpose:                      Print Hello World
+
+# Declaration of variables
+var-"hello world"
+# Declaration of functions
+
+
+# Main
+echo "hello world"
+echo $var
+
+# End
