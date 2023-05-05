5a38ef77860b1059e4aa201443a1aac921952088
Your Name <you@example.com>
2023-05-05 03:38:48 +0000

    second

---
+++ Batch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script Name:                  Batch.sh
+# Author:                       David Siebert 
+# Date of latest revision:      03MAY2023
+# Purpose:                      Batch
+
+
+# Declaration of variables
+
+# Declaration of functions still working here... 
