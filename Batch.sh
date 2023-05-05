ba6c6403379b850aa9c2d732ad85515798657db6
Your Name <you@example.com>
2023-05-05 15:46:42 +0000

    3rd commit

--- Batch.sh
+++ Batch.sh
@@ -3,9 +3,20 @@
 # Script Name:                  Batch.sh
 # Author:                       David Siebert 
 # Date of latest revision:      03MAY2023
-# Purpose:                      Batch
+# Purpose:                      Batch ( this is my test of concept. I dont know how to run this on the VM )
 
 
 # Declaration of variables
 
-# Declaration of functions still working here... 
+# Declaration of functions
+@echo off
+set source=C:\Users\Jorge\Desktop\Work
+set destination=D:\Backup\Jorge_Work
+
+echo Copying files from %source% to %destination%...
+echo.
+
+robocopy "%source%" "%destination%" /e /np /log:"D:\Backup\Jorge_Work\Backup_Log.txt"
+
+echo.
+echo Backup completed successfully.
