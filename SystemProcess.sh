ff717ea72102337d18fef232ea603a9b7bcf4731
Your Name <you@example.com>
2023-05-06 04:18:11 +0000

    first commit

---
+++ SystemProcess.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script Name:                  SystemProcess.sh
+# Author:                       David Siebert 
+# Date of latest revision:      05MAY2023
+# Purpose:                      Batch ( this is my test of concept. I dont know how to get this to run as I planned)
+
+
+# Declaration of variables
+
+# Declaration of functions
