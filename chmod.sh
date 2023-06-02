76ba71919088223be8ba23ec6d99c7dce46144a1
Your Name <you@example.com>
2023-06-02 15:39:12 +0000

    initial Commit

---
+++ chmod.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script Name:                  Batch.sh
+# Author:                       David Siebert 
+# Date of latest revision:      03MAY2023
+# Purpose:                      CHMOD 777
+
+
+# Declaration of variables
+
+# Declaration of functions
