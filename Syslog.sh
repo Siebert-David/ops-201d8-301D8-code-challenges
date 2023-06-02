eac4e47a6c1e9e9dbaadb746f66166bf22712f17
Your Name <you@example.com>
2023-06-02 21:23:26 +0000

    first commit

--- Syslog.sh
+++ Syslog.sh
@@ -4,3 +4,12 @@
 # Author:                       David Siebert 
 # Date of latest revision:      02JUN2023
 # Purpose:                      System log Move/Copy
+
+#cp /var/log/syslog .
+
+
+current date=$(date +"%Y-%m-%d_%h-%M-%S)
+
+cp /var/log/syslog "syslog_$(date +"%Y-%m-%d_%h-%M-%S")"
+
+#echo "syslog copied to $(pwd)"
