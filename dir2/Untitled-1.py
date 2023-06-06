c23f12568109cf5b27a61e35ad435f96d578e978
Your Name <you@example.com>
2023-06-06 17:19:12 +0000

    first commit

---
+++ dir2/Untitled-1.py
@@ -0,0 +1,33 @@
+#!/user/bin/env python
+
+# Script Name:                  Setup                
+# Class Name:                   Ops 301
+# Author Name:                  David Siebert
+# Date of latest revision:      06/06/2023
+# Creates:creates a python script that performs basic commands 
+
+
+mkdir hello
+cd hello
+code .
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
